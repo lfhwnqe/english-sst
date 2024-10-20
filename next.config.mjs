28d9accd811757af0b572b2619985385b14ba668
lfhwnqe <lfhwnqea@gmail.com>
2024-10-20 23:55:51 +0800

    feature: 增加代理配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*', // 本地 API 路径
+          destination: 'https://maomaocong.site/api/:path*', // 代理到远程 API
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
