b0c2b7282a5f705475f63e53e1c3bcbcb8c1f14c
lfhwnqe <lfhwnqea@gmail.com>
2024-10-20 23:55:43 +0800

    feature: 增加worker请求

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,37 @@ import { useState } from "react";
 export default function Home() {
   const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
   const [userInfo, setUserInfo] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
   const getUserInfo = () => {
     fetch(`${apiBaseUrl}/api/user`)
       .then((response) => response.json())
       .then((data) => {
-        console.log("response:", data);
-        setUserInfo(data);
+        setUserInfo(data.name);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+  const getProductsInfo = () => {
+    fetch(`${apiBaseUrl}/api/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        const products = data.products;
+        setList(products);
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
   return (
     <div>
       <button onClick={getUserInfo}>获取user数据</button>
-      <section>user数据区</section>
-      <button>获取products数据</button>
-      <section>products数据区</section>
+      <section>user数据区:{userInfo}</section>
+      <button onClick={getProductsInfo}>获取products数据</button>
+      <section>
+        products数据区:
+        <ul>
+          {list.map((i) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
