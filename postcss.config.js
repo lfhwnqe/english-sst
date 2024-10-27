8f8919090ab51bed7cd321408cec05e3eb116a4d
lfhwnqe <lfhwnqea@gmail.com>
2024-10-27 21:33:11 +0800

    feature: 修改配置文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+module.exports = config;
 
-export default config;
