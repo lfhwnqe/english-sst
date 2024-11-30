29ba9a6d6b5ed610f3735e66fa77817f522e3eca
lfhwnqe <lfhwnqea@gmail.com>
2024-11-30 20:44:16 +0800

    feature: 开发环境关闭pwa

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   runtimeCaching: require("./workbox-config.js"),
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
