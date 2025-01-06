501623c56afda91bfbf6bd49f756dac7919cd776
lfhwnqe <lfhwnqea@gmail.com>
2025-01-07 07:11:00 +0800

    feat: 修改alias

--- next.config.js
+++ next.config.js
@@ -5,12 +5,17 @@
 //   runtimeCaching: require("./workbox-config.js"),
 //   disable: process.env.NODE_ENV === "development",
 // });
-
+const path = require("path");
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
+  alias: {
+    "@": path.resolve(__dirname, "app"),
+    "@/app": path.resolve(__dirname, "app"),
+    "@/components": path.resolve(__dirname, "app/components"),
+  },
   async rewrites() {
     return [
       {
