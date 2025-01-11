71ee42d2f4abf78dc28dbf6c3b72ac79515313bd
lfhwnqe <lfhwnqea@gmail.com>
2025-01-11 22:01:47 +0800

    feat: 修改为测试链

--- app/config/contracts.ts
+++ app/config/contracts.ts
@@ -1,3 +1,6 @@
-export const courseMarketAddress = `0x04C89607413713Ec9775E14b954286519d836FEf`;
+export const MMCTokenAddress = `0xd4Ca987504bbAc09e6df1D4277c742cA43F044Ea`;
+export const courseMarketAddress = `0x2738704a2A91f2C2724422540f7991Ed0D144b75`;
 
-export const MMCTokenAddress = `0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d`;
+// export const courseMarketAddress = `0x04C89607413713Ec9775E14b954286519d836FEf`;
+
+// export const MMCTokenAddress = `0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d`;
