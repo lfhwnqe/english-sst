53cff239b616643e2b0636318ddb1806e2d6dd5f
lfhwnqe <lfhwnqea@gmail.com>
2025-01-18 18:04:47 +0800

    fix: 默认暗黑主题

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -4,6 +4,6 @@ export async function getAuthAndThemeFromCookie() {
   const cookieStore = await cookies();
   const hasTokenCookie = cookieStore.get("accessToken")?.value;
   const hasToken = hasTokenCookie ? true : false;
-  const theme = cookieStore.get("theme")?.value || "light";
+  const theme = cookieStore.get("theme")?.value || "dark";
   return { hasToken, theme };
 }
